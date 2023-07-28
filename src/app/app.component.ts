97fb2779697b234bddc2c2d62934534bdc0aff0f
Syracusa <msrndsy@gmail.com>
2023-07-28 17:28:18 +0900

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,29 @@ export class AppComponent {
         this.todo.push(result.task);
       });
   }
-  editTask(list: string, task: Task): void {}
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult|undefined) => {
+      if (!result) {
+        return;
+      }
+      console.log(this);
+      console.log(this[list]);
+      const dataList = this[list];
+      const taskIndex = dataList.indexOf(task);
+      if (result.delete) {
+        dataList.splice(taskIndex, 1);
+      } else {
+        dataList[taskIndex] = task;
+      }
+    });
+  }
 
   drop(event: CdkDragDrop<Task[]>): void {
     if (event.previousContainer === event.container) {
